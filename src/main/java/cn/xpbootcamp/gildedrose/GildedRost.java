12196dd722a74dc6f37ec795e3dae13e0b7cc166
daolang.huang <daolang.huang@thoughtworks.com>
2020-05-17 11:54:02 +0800

    Tasking 2 is ok

--- src/main/java/cn/xpbootcamp/gildedrose/GildedRost.java
+++ src/main/java/cn/xpbootcamp/gildedrose/GildedRost.java
@@ -5,7 +5,13 @@ public class GildedRost {
         int price = 0;
         switch (commodity.rule) {
             case normal:
-                price = (int) Math.round(commodity.quality * Math.pow(0.95, afterDays));
+                if (afterDays <= 10) {
+                    price = (int) Math.round(commodity.quality * Math.pow(0.95, afterDays));
+                } else {
+                    Double tenDaysPrice = commodity.quality * Math.pow(0.95, 10);
+                    price = (int) Math.round(tenDaysPrice * Math.pow(0.9, afterDays - 10));
+                }
+
                 break;
             default:
                 break;
--- src/test/java/cn/xpbootcamp/gildedrose/GildedRoseTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/GildedRoseTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GildedRoseTest {
     GildedRost rost = new GildedRost();
     @org.junit.jupiter.api.Test
-    void should_return_30_when_after_ten_days_given_rule_is_normal_sellIn_is_10_quality_is_50() {
+    void should_return_30_when_after_10_days_given_rule_is_normal_sellIn_is_10_quality_is_50() {
         Commodity commodity = new Commodity();
         commodity.name = "Some";
         commodity.quality = 50;
@@ -14,5 +14,16 @@ public class GildedRoseTest {
         commodity.rule = PriceChangeRule.normal;
 
         assertEquals(30, rost.priceChangedBy(commodity, 10));
+    }
+
+    @org.junit.jupiter.api.Test
+    void should_return_24_when_after_12_days_given_rule_is_normal_sellIn_is_10_quality_is_50() {
+        Commodity commodity = new Commodity();
+        commodity.name = "Some";
+        commodity.quality = 50;
+        commodity.sellIn = 10;
+        commodity.rule = PriceChangeRule.normal;
+
+        assertEquals(24, rost.priceChangedBy(commodity, 12));
     }
 }
